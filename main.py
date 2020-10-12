4d671d8522c58f73e475747a19cd0507177cfb95
[Students] Maikls Deivids Beginskis <91DPMDBeginskis@rvt.lv>
2020-10-12 11:25:40 +0300

    templates

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Sveika, pasaule</h1>'
+    return '<h1>Sveika, pasaule test</h1>'
 
-app.run(host='0.0.0.0', port=80)
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+app.run(host='0.0.0.0', port=80, debug=True)
